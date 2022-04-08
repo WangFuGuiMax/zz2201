b1926cdbf4fcd2a5067fdc7f722d8fd62dba648f
Wangfugui <1139771244@qq.com>
2022-04-08 21:05:56 +0800

    啦啦啦

---
+++ jjjjdddd/gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const connect = require("gulp-connect");
+const sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("copyHtml", done => {
+  gulp.src("*.html").pipe(gulp.dest("dist")).pipe(connect.reload());
+  done();
+})
+gulp.task("copycsan", done => {
+  gulp.src("css/*.css").pipe(gulp.dest("dist/css")).pipe(connect.reload());
+  done();
+})
+gulp.task("copyimg", done => {
+  gulp.src("img/*.{jpg,png}").pipe(gulp.dest("dist/img")).pipe(connect.reload());
+  done();
+})
+gulp.task("sass", done => {
+  gulp.src("sass/*.scss")
+    .pipe(sourcemaps.init())
+    /* .pipe(sass({
+      outputStyle: "compressed"
+    })) */
+    .pipe(sass())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("dist/css")).pipe(connect.reload());
+  done();
+})
+gulp.task("server", done => {
+  connect.server({
+    root: "dist",
+    livereload: true
+  })
+  done();
+})
+gulp.task("watch", done => {
+  gulp.watch("*.html", gulp.series("copyHtml"));
+  gulp.watch("css/*.css", gulp.series("copycsan"));
+  gulp.watch("img/*.{png,jpg}", gulp.series("copyimg"));
+  gulp.watch("sass/*.scss", gulp.series("sass"));
+  done();
+})
+
+gulp.task("default", gulp.series("server", "watch"));
